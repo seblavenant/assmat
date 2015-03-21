562895eecd569418b57695e8aecca586f098e87d
seblavenant <sebastien@lavenant.me>
2015-03-21 22:26:13 +0100

    chore: add xdebug vagrant install

--- .vagrant/provision/bootstrap.sh
+++ .vagrant/provision/bootstrap.sh
@@ -12,7 +12,11 @@ apt-get update
 # INSTALL APACHE / PHP
 apt-get install -y apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
 cp /vagrant/.vagrant/provision/apache2/virtualhost.conf /etc/apache2/sites-enabled/
-apt-get install -y libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+apt-get install -y libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl 
+
+# XDEBUG
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sh /vagrant/.vagrant/provision/apache2/xdebug.sh
 
 # INSTALL OTHER SOFTWARE
 apt-get install -y git
@@ -26,9 +30,3 @@ source /home/vagrant/.bashrc
 
 # SERVICE LAUNCH
 service apache2 restart
-
-# APPLICATION
-cd /var/www
-php composer.phar install
-vendor/bin/karma hydrate
-cd -
