4216062495896507ce5445fd30f8490112f0ad62
seblavenant <sebastien@lavenant.me>
2015-03-26 11:35:04 +0100

    feat: add sql for employe table creation

---
+++ src/DataSource/Schemas/employe.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `employe` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `contact_id` int(11) NOT NULL,
+ `ss_id` varchar(128) NOT NULL,
+ PRIMARY KEY (`id`),
+ KEY `contact_id` (`contact_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
