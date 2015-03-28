8fa69edad7497e31d1496b1a5bc3bf0679fb200b
seblavenant <sebastien@lavenant.me>
2015-03-28 11:47:50 +0100

    vagrant: enable mod rewrite

--- .vagrant/provision/bootstrap.sh
+++ .vagrant/provision/bootstrap.sh
@@ -14,6 +14,8 @@ apt-get install -y apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpa
 cp /vagrant/.vagrant/provision/apache2/virtualhost.conf /etc/apache2/sites-enabled/
 apt-get install -y libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl 
 
+a2enmod rewrite
+
 # XDEBUG
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 sh /vagrant/.vagrant/provision/apache2/xdebug.sh
