f6e67e7c161e235f0761728627396ae55f40a322
seblavenant <sebastien@lavenant.me>
2015-04-29 22:30:11 +0200

    chore: compute salaire ligne before other events

--- src/Services/Lignes/Builder.php
+++ src/Services/Lignes/Builder.php
@@ -5,6 +5,9 @@ namespace Assmat\Services\Lignes;
 use Assmat\DataSource\Domains;
 use Assmat\DataSource\DataTransferObjects as DTO;
 use Assmat\DataSource\Repositories;
+use Assmat\DataSource\Constants;
+use Assmat\Iterators\Filters as FilterIterator;
+use Assmat\DataSource\Repositories\Memory\Lignes\Salaire;
 
 class Builder
 {
@@ -20,16 +23,36 @@ class Builder
     {
         $lignes = $this->ligneRepository->findFromCodes($codes);
 
-        if(empty($lignes))
+        if(iterator_count($lignes) === 0)
         {
             return;
         }
 
+        $ligneSalaire = $this->getLigneSalaire($lignes);
+        $ligneSalaire->hydrateFromBulletin($bulletin);
+
         foreach($lignes as $ligne)
         {
-            $ligne->getValeur($bulletin);
+            if($ligne->getCode() === Constants\Lignes\Code::SALAIRE)
+            {
+                continue;
+            }
+
+            $ligne->hydrateFromBulletin($bulletin);
         }
 
         return $lignes;
+    }
+
+    private function getLigneSalaire($lignes)
+    {
+        $ligneSalaireIterator = new FilterIterator\Lignes\Code($lignes, array(Constants\Lignes\Code::SALAIRE));
+
+        if(iterator_count($ligneSalaireIterator) === 0)
+        {
+            throw new \Exception('La ligne salaire est requise !');
+        }
+
+        return current(iterator_to_array($ligneSalaireIterator));
     }
 }
