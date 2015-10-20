8bf1c332b0dffca25c72828f5ac6440b9bed55d1
Sébastien LAVENANT <sebastien@lavenant.me>
2015-10-20 14:42:22 +0200

    feat: add missing fields in contrat create form

--- src/DataSource/Forms/Contrat.php
+++ src/DataSource/Forms/Contrat.php
@@ -6,9 +6,20 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Validator\Constraints;
+use Assmat\DataSource\Constants;
+use Assmat\DataSource\Repositories;
+use Assmat\DataSource\Domains;
 
 class Contrat extends AbstractType
 {
+    private
+        $employesRepository;
+
+    public function __construct(Repositories\Employe $employeRepository)
+    {
+        $this->employeRepository = $employeRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -17,14 +28,65 @@ class Contrat extends AbstractType
                 'required' => true,
                 'constraints' => array(
                      new Constraints\NotBlank(),
-                     new Constraints\Length(array('min' => 5)),
                  ),
             ))
-            ->add('salaireHoraire', 'number')
-            ->add('joursGarde', 'integer')
-            ->add('heuresHebdo', 'integer')
-            ->add('typeId')
-            ->add('contratId')
+            ->add('salaireHoraire', 'text', array(
+                'label' => 'contrats.salaireHoraire',
+                'required' => true,
+                'constraints' => array(
+                    new Constraints\NotBlank(),
+                    new Constraints\Regex(array(
+                        'pattern' => '/^\d*\.?\d*$/i',
+                        'message' => 'Le format du montant est invalide (attendu 0.00)'
+                    )),
+                )
+            ))
+            ->add('joursGarde', 'choice', array(
+                'label' => 'contrats.joursGarde',
+                'required' => true,
+                'choices' => range(1, 7),
+                'placeholder' => '-- Sélectionnez un nombre de jours --',
+                'constraints' => array(
+                    new Constraints\NotBlank(),
+                )
+            ))
+            ->add('heuresHebdo', 'integer', array(
+                'label' => 'contrats.heuresHebdo',
+                'required' => true,
+                'constraints' => array(
+                    new Constraints\NotBlank(),
+                    new Constraints\Range(array('min' => 0, 'max' => 99)),
+                )
+            ))
+            ->add('nombreSemainesAn', 'integer', array(
+                'label' => 'contrats.nombreSemainesAn',
+                'required' => true,
+                'constraints' => array(
+                    new Constraints\NotBlank(),
+                    new Constraints\Range(array('min' => 0, 'max' => 52)),
+                )
+            ))
+            ->add('typeId', 'choice', array(
+                'label' => 'contrats.typeId',
+                'required' => true,
+                'choices' => array(
+                    Constants\Contrats\Salaire::MENSUALISE => 'Mensualisé',
+                ),
+                'placeholder' => '-- Sélectionnez un type de contrat --',
+                'constraints' => array(
+                    new Constraints\NotBlank(),
+                )
+            ))
+            ->add('employeId', 'choice', array(
+                 'label' => 'contrats.employeId',
+                 'required' => true,
+                 'choices' => $this->retrieveEmployees($options),
+                 'placeholder' => '-- Sélectionnez un employé --',
+                 'constraints' => array(
+                      new Constraints\NotBlank(),
+                 )
+            ))
+            ->add('employeurId', 'hidden')
             ;
     }
 
@@ -36,7 +98,28 @@ class Contrat extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'csrf_protection' => false,
+            'employeur' => null,
         ));
+    }
+
+    private function retrieveEmployees(array $options)
+    {
+        if(! array_key_exists('employeur', $options) || ! $options['employeur'] instanceof Domains\Employeur)
+        {
+            return array();
+        }
+
+        $employes = $this->employeRepository->findFromEmployeur($options['employeur']->getId());
+
+        $employesArray = array();
+        foreach($employes as $employe)
+        {
+            if($employe instanceof Domains\Employe)
+            {
+                $employesArray[$employe->getId()] = sprintf('%s %s', $employe->getContact()->getPrenom(), $employe->getContact()->getNom());
+            }
+        }
+
+        return $employesArray;
     }
 }
