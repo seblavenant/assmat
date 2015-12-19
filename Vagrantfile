05a89ded56b9aac62e6dc31038024b6ce78fa01c
Sébastien LAVENANT <sebastien@lavenant.me>
2015-12-19 09:25:00 +0100

    chore: export provision script out of vagrant dir

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.vm.box = "debian/wheezy64"
-  config.vm.provision :shell, path: ".vagrant/provision/bootstrap.sh"
+  config.vm.provision :shell, path: "provision/bootstrap.sh"
   #config.vm.network :forwarded_port, host: 8080, guest: 80
   config.vm.network "private_network", ip: "192.168.50.4"
 
--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -11,23 +11,23 @@ apt-get update
 
 # INSTALL APACHE / PHP
 apt-get install -y apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
-cp /vagrant/.vagrant/provision/apache2/virtualhost.conf /etc/apache2/sites-enabled/
+cp /vagrant/provision/apache2/virtualhost.conf /etc/apache2/sites-enabled/
 apt-get install -y libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl 
 
 a2enmod rewrite
 
 # XDEBUG
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-sh /vagrant/.vagrant/provision/apache2/xdebug.sh
+sh /vagrant/provision/apache2/xdebug.sh
 
 # INSTALL OTHER SOFTWARE
 apt-get install -y git
 apt-get install -y vim
 
 # CONFIG USER
-cp /vagrant/.vagrant/provision/home/.bashrc /home/vagrant/
-cp /vagrant/.vagrant/provision/home/.gitconfig /home/vagrant/
-cp /vagrant/.vagrant/provision/home/.vimrc /home/vagrant/
+cp /vagrant/provision/home/.bashrc /home/vagrant/
+cp /vagrant/provision/home/.gitconfig /home/vagrant/
+cp /vagrant/provision/home/.vimrc /home/vagrant/
 source /home/vagrant/.bashrc
 
 # SERVICE LAUNCH
@@ -41,5 +41,5 @@ apt-get -y install mysql-server
 apt-get install -y wkhtmltopdf
 apt-get install -y openssl build-essential xorg libssl-dev
 apt-get install -y xvfb
-cp /vagrant/.vagrant/provision/scripts/wkhtmltopdf.sh /usr/local/bin/
+cp /vagrant/provision/scripts/wkhtmltopdf.sh /usr/local/bin/
 chmod a+x /usr/local/bin/wkhtmltopdf.sh
