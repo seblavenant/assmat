653111990a9fecad63fd774f8da7cce8574880c1
Sébastien LAVENANT <sebastien@lavenant.me>
2016-06-05 11:26:22 +0200

    fix: path to docker-compose generated config

--- .deploy/scripts/docker.sh
+++ .deploy/scripts/docker.sh
@@ -9,4 +9,4 @@ echo '    - Copy router conf'
 cp "$(pwd)/../config/vhosts/router/assmat.conf" "$SYSTEM_PATH/var/confs/"
 
 echo '    - Deploy containers'
-cat "$(pwd)/../config/docker/docker-compose.yml" >> "$SYSTEM_PATH/var/docker/docker-compose.yml"
+cat "$(pwd)/../../../var/config/docker-compose.yml" >> "$SYSTEM_PATH/var/docker/docker-compose.yml"
