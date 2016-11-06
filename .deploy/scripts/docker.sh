faf78994f554541e45b249bdf18e7508ae1e97d6
Sébastien LAVENANT <sebastien@lavenant.me>
2016-11-06 16:33:32 +0100

    chore: move hydrated conf to a specific dir

--- .deploy/scripts/docker.sh
+++ .deploy/scripts/docker.sh
@@ -9,4 +9,4 @@ echo '    - Copy router conf'
 cp "$(pwd)/../config/vhosts/router/assmat.conf" "$SYSTEM_PATH/var/confs/"
 
 echo '    - Deploy containers'
-cat "$(pwd)/../../../var/config/docker-compose.yml" >> "$SYSTEM_PATH/var/docker/docker-compose.yml"
+cat "$(pwd)/../../config/hydrated/docker-compose.yml" >> "$SYSTEM_PATH/var/docker/docker-compose.yml"
